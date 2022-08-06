3ee673033659bee7882bb9e7ea7d3b739a40f675
DarkWoodX <115884396+DarkWoodX@users.noreply.github.com>
2022-08-06 22:38:00 +0000

    * data update (minor)

---
+++ chrome-extension/content.js
@@ -0,0 +1,7 @@
+
+﻿// TALK TO CHATGPT
+// ---------------
+// Author		: C. NEDELCU
+// Version		: 1.6.1
+// Git repo 	: https://github.com/C-Nedelcu/talk-to-chatgpt
+// Chat GPT URL	: https://chat.openai.com/chat
