61a35bf1350da8f7b0feb05d6e453b249b809e75
DarkWoodX <115884396+DarkWoodX@users.noreply.github.com>
2022-08-08 11:47:56 +0000

    in development

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -5,3 +5,9 @@
 // Version		: 1.6.1
 // Git repo 	: https://github.com/C-Nedelcu/talk-to-chatgpt
 // Chat GPT URL	: https://chat.openai.com/chat
+// How to use   : https://www.youtube.com/watch?v=VXkLQMEs3lA
+
+
+// ----------------------------
+// SETTINGS (FEEL FREE TO EDIT)
+// ----------------------------
