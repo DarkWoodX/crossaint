88472a95e116ec435b2b5f906a826cde4e012604
DarkWoodX <115884396+DarkWoodX@users.noreply.github.com>
2022-08-08 20:52:03 +0000

    Force push

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -16,3 +16,17 @@
 
 // Settings for the text-to-speech functionality (the bot's voice)
 var CN_TEXT_TO_SPEECH_RATE = 1; // The higher the rate, the faster the bot will speak
+var CN_TEXT_TO_SPEECH_PITCH = 1; // This will alter the pitch for the bot's voice
+
+// Indicate a locale code such as 'fr-FR', 'en-US', to use a particular language for the speech recognition functionality (when you speak into the mic)
+// If you leave this blank, the system's default language will be used
+var CN_WANTED_LANGUAGE_SPEECH_REC = ""; //"fr-FR";
+
+// Determine which word will cause this scrip to stop.
+var CN_SAY_THIS_WORD_TO_STOP = "stop";
+
+// Determine which word will cause this script to temporarily pause
+var CN_SAY_THIS_WORD_TO_PAUSE = "pause";
+
+// Determine whether messages are sent immediately after speaing
+var CN_AUTO_SEND_AFTER_SPEAKING = true;
